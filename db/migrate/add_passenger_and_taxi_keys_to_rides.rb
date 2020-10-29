172a14aa266d67213d0a85ef699cf96556a3c980
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-29 06:20:05 +0000

    Automatically backed up by Learn

--- db/migrate/add_passenger_and_taxi_keys_to_rides.rb
+++ db/migrate/add_passenger_and_taxi_keys_to_rides.rb
@@ -1,4 +1,4 @@
-class AddPassengerAndTaxiKeysToRides < ActiveRecord::Migration[5.0]
+class AddPassengerAndTaxiKeysToRides < ActiveRecord::Migration
   def change
     add_column :rides, :passenger_id, :integer
     add_column :rides, :taxi_id, :integer
