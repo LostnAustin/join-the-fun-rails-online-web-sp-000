7da7dc9d4cdf5564ff4ccbfd621323962098241f
Chad Lengefeld <chadlengefeld@gmail.com>
2020-10-29 06:05:18 +0000

    Automatically backed up by Learn

--- db/migrate/add_passenger_and_ride_id_to_rides.rb
+++ db/migrate/add_passenger_and_ride_id_to_rides.rb
@@ -2,7 +2,7 @@ class AddPassengerAndTaxiIdToRides < ActiveRecord::Migration[5.0]
   def change
     add_column : do |t|
       t.integer :passenger_id
-      t.integer
+      t.integer :taxi_id
 
   end
 
